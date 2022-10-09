641ea8f14603d8f7d9547ee683fab713bea352d7
zyf1169013790 <2314048826@qq.com>
2022-10-09 22:41:46 +0800

    ceshi

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -7,7 +7,16 @@ package org.example;
  * @Date 2022/10/9 22:29
  */
 public class Test {
+    /**
+     * @MethodName: main
+     * @Params:
+       * @param: args  [java.lang.String]
+     * @return: void
+     * @Auther: zyf
+     * @Date: 2022/10/9 22:38
+     * @Description: 主方法
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!!");
     }
 }
