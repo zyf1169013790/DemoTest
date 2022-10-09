4c244ad5c34e923f5139a25707357b52f527682f
zyf1169013790 <2314048826@qq.com>
2022-10-09 22:35:41 +0800

    Initial commit

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @ClassName Test
+ * @Description Test
+ * @Author zyf
+ * @Date 2022/10/9 22:29
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
